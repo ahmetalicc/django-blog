d3fdd5f02bbaa179d23e27fef644d55613285e29
Ahmet Alıç <ahmetskry54@gmail.com>
2025-03-17 11:15:11 +0300

    Use admin.register decorator

--- blog/admin.py
+++ blog/admin.py
@@ -3,23 +3,21 @@ from blog.models import CategoryModel, ArticleModel, CommentModel, ContactModel
 
 admin.site.register(CategoryModel)
 
+@admin.register(ArticleModel)
 class ArticleAdmin(admin.ModelAdmin):
     search_fields = ("title", "content")
     list_display = (
         "title", "createdAt", "updatedAt"
     )
 
-admin.site.register(ArticleModel, ArticleAdmin)
-
+@admin.register(CommentModel)
 class CommentAdmin(admin.ModelAdmin):
     search_fields = ("author__username",)
     list_display = ("author", "createdAt" , "updatedAt")
 
-admin.site.register(CommentModel, CommentAdmin)
-
+@admin.register(ContactModel)
 class ContactAdmin(admin.ModelAdmin):
     search_fields = ("email",)
     list_display = ("email", "createdAt")
 
-admin.site.register(ContactModel, ContactAdmin)
 
